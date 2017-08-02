a80a1a43a8b797d74ae4860cd80d8afbb0ba6458
saboohajaz <sabooh@gmail.com>
2017-08-02 18:37:03 +0930

    2nd compiler

--- Projects/Multi/Applications/Contiki/Udp-receiver/EWARM/spml/settings/Project.STM32L152RE_Nucleo.cspy.bat
+++ Projects/Multi/Applications/Contiki/Udp-receiver/EWARM/spml/settings/Project.STM32L152RE_Nucleo.cspy.bat
@@ -25,7 +25,7 @@ if not "%~1" == "" goto debugFile
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.5\common\bin\cspybat" -f "C:\Users\Sabooh\Documents\GitHub\sprtmodule\Projects\Multi\Applications\Contiki\Udp-receiver\EWARM\STM32L152RE-Nucleo\settings\Project.STM32L152RE_Nucleo.general.xcl" --backend -f "C:\Users\Sabooh\Documents\GitHub\sprtmodule\Projects\Multi\Applications\Contiki\Udp-receiver\EWARM\STM32L152RE-Nucleo\settings\Project.STM32L152RE_Nucleo.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.5\common\bin\cspybat" -f "C:\Users\Sabooh\Documents\GitHub\sprt_wrking\Projects\Multi\Applications\Contiki\Udp-receiver\EWARM\spml\settings\Project.STM32L152RE_Nucleo.general.xcl" --backend -f "C:\Users\Sabooh\Documents\GitHub\sprt_wrking\Projects\Multi\Applications\Contiki\Udp-receiver\EWARM\spml\settings\Project.STM32L152RE_Nucleo.driver.xcl" 
 
 @echo off 
 goto end 
@@ -34,7 +34,7 @@ goto end
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.5\common\bin\cspybat" -f "C:\Users\Sabooh\Documents\GitHub\sprtmodule\Projects\Multi\Applications\Contiki\Udp-receiver\EWARM\STM32L152RE-Nucleo\settings\Project.STM32L152RE_Nucleo.general.xcl" "--debug_file=%~1" --backend -f "C:\Users\Sabooh\Documents\GitHub\sprtmodule\Projects\Multi\Applications\Contiki\Udp-receiver\EWARM\STM32L152RE-Nucleo\settings\Project.STM32L152RE_Nucleo.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.5\common\bin\cspybat" -f "C:\Users\Sabooh\Documents\GitHub\sprt_wrking\Projects\Multi\Applications\Contiki\Udp-receiver\EWARM\spml\settings\Project.STM32L152RE_Nucleo.general.xcl" "--debug_file=%~1" --backend -f "C:\Users\Sabooh\Documents\GitHub\sprt_wrking\Projects\Multi\Applications\Contiki\Udp-receiver\EWARM\spml\settings\Project.STM32L152RE_Nucleo.driver.xcl" 
 
 @echo off 
 :end
